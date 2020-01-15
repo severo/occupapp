637c67b0eda1e724f6f31d4739eb0b9ae4c9c755
Sylvain Lesage <severo@rednegra.net>
2020-01-15 11:19:19 +0100

    feat: add current composition spec to tables panel

---
+++ src/components/SpecJSON.vue
@@ -0,0 +1,22 @@
+<template>
+  <pre><code>{{ jsonString }}</code></pre>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { getModule } from 'vuex-module-decorators'
+
+import { compositionToUrlQuerySpec } from '@/utils/composition.ts'
+import Compositions from '@/store/compositions.ts'
+
+const compositions = getModule(Compositions)
+
+@Component
+export default class SpecJSON extends Vue {
+  get jsonString () {
+    return JSON.stringify(compositionToUrlQuerySpec(compositions.current), null, 2)
+  }
+}
+
+</script>
--- src/components/TablesPanel.vue
+++ src/components/TablesPanel.vue
@@ -1,18 +1,31 @@
 <template>
   <v-container>
     <h2 class="title mt-4 mb-2">
-      Points Table
+      Current composition
     </h2>
+
+    <h3 class="subtitle-1 mt-4 mb-2">
+      Points
+    </h3>
     <PointsTable />
 
-    <h2 class="title mt-4 mb-2">
-      Categories Table
-    </h2>
+    <h3 class="subtitle-1 mt-4 mb-2">
+      Categories
+    </h3>
     <CategoriesTable />
 
+    <h3 class="subtitle-1 mt-4 mb-2">
+      Spec
+    </h3>
+    <SpecJSON />
+
     <h2 class="title mt-4 mb-2">
-      State (JSON)
+      Full internal state
     </h2>
+
+    <h3 class="subtitle-1 mt-4 mb-2">
+      State
+    </h3>
     <StoreJSON />
   </v-container>
 </template>
@@ -28,12 +41,14 @@ import Component from 'vue-class-component'
 import CategoriesTable from '@/components/CategoriesTable.vue'
 import PointsTable from '@/components/PointsTable.vue'
 import StoreJSON from '@/components/StoreJSON.vue'
+import SpecJSON from '@/components/SpecJSON.vue'
 
 @Component({
   components: {
     CategoriesTable,
     PointsTable,
-    StoreJSON
+    StoreJSON,
+    SpecJSON
   }
 })
 export default class TablesPanel extends Vue {
